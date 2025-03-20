43b886991a32748b8e490429f2a26812d3c9dc7f
sunew130 <sunew130@gmail.com>
2025-03-20 22:13:55 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="Zlibrary-API",
+    version="0.1",
+    py_modules=["Zlibrary"],
+    install_requires=["requests"],
+    description="Unofficial Z-Library API wrapper",
+    url="https://github.com/sunew130/Zlibrary-API",
+)
